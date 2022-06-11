27f3c6d79893d68114627a4cf316dcfb05e86fd1
Jehu <ijehu.silva@gmail.com>
2022-06-11 18:47:04 -0500

    Modifing the entrypoint for development environment to drops all existing tables and then creates the tables from the models, every time the container is run

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,16 +1,8 @@
-#!/bin/sh
-
-if [ "$DATABASE" = "postgres" ]
+if [ "$FLASK_ENV" = "development" ]
 then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
+    echo "Creating the database tables..."
+    python manage.py create_db
+    echo "Tables created"
 fi
-
-python manage.py create_db
 
 exec "$@"

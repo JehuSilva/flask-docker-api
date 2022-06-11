29f449543814932564c005b774f3b940905b0baf
Jehu <ijehu.silva@gmail.com>
2022-06-11 18:51:54 -0500

    Updating entrypoint for production environment

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-# Development steps definition
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
 if [ "$FLASK_ENV" = "development" ]
 then
-    # It drops all existing tables and then creates the tables from the models, 
-    #every time the container is run
     echo "Creating the database tables..."
     python manage.py create_db
     echo "Tables created"

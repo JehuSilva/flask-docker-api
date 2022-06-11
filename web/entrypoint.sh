fc82ced0cc7a0e59852c4b1b794e6ec4e0d1e56a
Jehu <ijehu.silva@gmail.com>
2022-06-11 18:48:09 -0500

    Modifing the entrypoint for development environment to drops all existing tables and then creates the tables from the models, every time the container is run

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,5 +1,10 @@
+#!/bin/sh
+
+# Development steps definition
 if [ "$FLASK_ENV" = "development" ]
 then
+    # It drops all existing tables and then creates the tables from the models, 
+    #every time the container is run
     echo "Creating the database tables..."
     python manage.py create_db
     echo "Tables created"

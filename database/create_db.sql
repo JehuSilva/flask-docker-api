12dfc0c180240fe3ad00ba1b83643faa9b27c7b4
Jehu <ijehu.silva@gmail.com>
2022-06-17 02:13:53 -0500

    Set default schema name to sanbox

--- database/create_db.sql
+++ database/create_db.sql
@@ -1,17 +1,19 @@
-DROP SCHEMA IF EXISTS directory CASCADE;
+-- This script is used to generate the schema in a PostgreSQL database.
 
-CREATE SCHEMA directory;
+DROP SCHEMA IF EXISTS sandbox CASCADE;
 
-CREATE TABLE directory.employee (
+CREATE SCHEMA sandbox;
+
+CREATE TABLE sandbox.employee (
     employee_id serial PRIMARY KEY,
     first_name varchar(50) NOT NULL,
     last_name varchar(50) NOT NULL,
     email varchar(50) UNIQUE NOT NULL,
     phone1 varchar(50) NOT NULL,
-    phone2 varchar(50)
+    phone2 varchar(50) NOT NULL
 );
 
-CREATE TABLE directory.company (
+CREATE TABLE sandbox.company (
     company_id serial PRIMARY KEY,
     name varchar(50) UNIQUE NOT NULL,
     address varchar(50) NOT NULL,
@@ -20,21 +22,21 @@ CREATE TABLE directory.company (
     zip varchar(50) NOT NULL
 );
 
-CREATE TABLE directory.department (
+CREATE TABLE sandbox.department (
     department_id serial PRIMARY KEY,
     name varchar(50) UNIQUE NOT NULL
 );
 
-CREATE TABLE directory.employee_company(
+CREATE TABLE sandbox.employee_company(
     id serial PRIMARY KEY,
     employee_id INT NOT NULL,
     company_id INT NOT NULL,
     department_id INT NOT NULL,
 UNIQUE (employee_id, company_id, department_id),
 FOREIGN KEY (employee_id)
-    REFERENCES directory.employee (employee_id),
+    REFERENCES sandbox.employee (employee_id),
 FOREIGN KEY (company_id)
-    REFERENCES directory.company (company_id),
+    REFERENCES sandbox.company (company_id),
 FOREIGN KEY (department_id)
-    REFERENCES directory.department (department_id)
+    REFERENCES sandbox.department (department_id)
 );
